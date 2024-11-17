d56da94aab1bbbd9524ee0b421dca02687df4d9c
Sagi Barshai <sagibarshai1@gmail.com>
2024-11-17 21:43:09 +0200

    make some of the signup fields to be optinal

--- backend/src/features/auth/sign-up/route.ts
+++ backend/src/features/auth/sign-up/route.ts
@@ -8,15 +8,15 @@ const router = Router();
 
 router.post(
   "/signUp",
-  body("firstName").isLength({ min: 2, max: 40 }).withMessage("First name should be exist with 2 - 40 characters"),
-  body("lastName").isLength({ min: 2, max: 40 }).withMessage("Last name should be exist with 2 - 40 characters"),
+  body("firstName").optional().isLength({ min: 2, max: 40 }).withMessage("First name should be with 2 - 40 characters"),
+  body("lastName").optional().isLength({ min: 2, max: 40 }).withMessage("Last name should be with 2 - 40 characters"),
+  body("phoneNumber").optional().matches(formattedPhoneRegex).withMessage("Phone number must be in this structure : +YYYYXXXXXXXX"),
   body("email").isEmail().withMessage("Email should be exist and in a valid structure"),
   body("password")
     .isStrongPassword({ minLowercase: 1, minNumbers: 1, minSymbols: 1, minUppercase: 1 })
     .withMessage("Password should contain at least 1 symbol, 1 uppercase, 1 lowercase, 1 number")
     .isLength({ min: 6, max: 30 })
     .withMessage("Password should be exist with 6 - 30 characters"),
-  body("phoneNumber").matches(formattedPhoneRegex).withMessage("Phone number must be in this structure : +YYYYXXXXXXXX"),
   requestValidationMiddleware,
   signUpController
 );
